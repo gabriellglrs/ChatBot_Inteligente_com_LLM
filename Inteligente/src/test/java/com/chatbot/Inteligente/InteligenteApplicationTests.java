fff89116e1e0b1d4fb3b7245a1b05483d193b480
gabriellglrs <gabriellglrs@gmail.com>
2025-03-10 20:57:16 -0300

    feat: inicio do projeto

---
+++ Inteligente/src/test/java/com/chatbot/Inteligente/InteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.Inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
